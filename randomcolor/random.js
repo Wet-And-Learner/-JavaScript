c190c7b757048dd485b6ad12376deec7dcd49d1d
chomu durga <chomu durga@chomudurga.chomudurga>
2023-05-31 19:28:23 +0530

    by wet

--- randomcolor/random.js
+++
@@ -1,8 +0,0 @@
-const tb=document.getElementById('tab')
-
-tb.addEventListener('click',function(){
-    const randomcolours=['red','blue','green','black','orange','red','orangered','gray','grey']
-    const len=randomcolours.length-1
-    const rc= randomcolours[Math.floor(Math.random()*len)]
-    tb.style.background=rc
-})
